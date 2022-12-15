b2c53274663b90b0cb370de5b57359e1cdb9f264
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-15 19:29:20 +0530

    Create 102) Javascript Alerts - Adding Behaviour to Websites.js

---
+++ 9) Introduction to Javascript ES6/102) Javascript Alerts - Adding Behaviour to Websites.js
@@ -0,0 +1,9 @@
+alert("hello");       // This will disply one pop-up with "hello" message.  In this "alert" is a fuction, "hello" is message and ";" is fullstop or end.
+alert("world!");      // After, clicking OK for previous pop-up, I get this pop-up "world!"
+say("hello");         // This will give an error, because "say" is not a keyword or function in javascript.
+
+// To check which are valid keyword or not, simply go to "MDN docs" website, there click on javascript and search in searchbar for specific keyword.
+
+// Syntax is grammer for Programming language Like same as English language grammer.
+
+// Use this link to refer  proper coding style or common coding style of javascript. "https://github.com/rwaldron/idiomatic.js"
