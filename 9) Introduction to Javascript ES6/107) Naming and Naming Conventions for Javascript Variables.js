716c8be7cbc8f7dffa5aaa431cb0789e5bd70508
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-16 10:13:46 +0530

    Create 107) Naming and Naming Conventions for Javascript Variables.js

---
+++ 9) Introduction to Javascript ES6/107) Naming and Naming Conventions for Javascript Variables.js
@@ -0,0 +1,9 @@
+// We have to follow some Naming convention, and we should try to give matching name for data. The name should explain, what data it contains.
+//These are some valid variable name :
+var myName = "Prasad";    
+var yourName = "Kaju";
+var myvar = "abc";
+var my123 = 123;          // we can't use varibale name "123".
+var myname = "Angela";
+var my_name = "Angela";   // we can't use varibale name "my-name"
+var userScoreFinal = 12;  // This is Camel case variable name.
