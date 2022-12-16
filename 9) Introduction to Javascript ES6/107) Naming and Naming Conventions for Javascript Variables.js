a1490b60451e0fb7c7ab6efbb626bcef18632c47
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-16 10:22:41 +0530

    Update 107) Naming and Naming Conventions for Javascript Variables.js

--- 9) Introduction to Javascript ES6/107) Naming and Naming Conventions for Javascript Variables.js
+++ 9) Introduction to Javascript ES6/107) Naming and Naming Conventions for Javascript Variables.js
@@ -4,6 +4,6 @@ var myName = "Prasad";
 var yourName = "Kaju";
 var myvar = "abc";
 var my123 = 123;          // we can't use varibale name "123".
-var myname = "Angela";
+var myname = "Angela";    // "my name" this not valid variable name, because we can't give space in between of variable name. If we give then it treats first word as Variable name and for another word it give error, because it doesn't know what it is.
 var my_name = "Angela";   // we can't use varibale name "my-name"
 var userScoreFinal = 12;  // This is Camel case variable name.
