057c408ad342843256b5edb8bddae2716bd1416f
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-16 08:07:22 +0530

    Create 103) Data Types.js

---
+++ 9) Introduction to Javascript ES6/103) Data Types.js
@@ -0,0 +1,17 @@
+// How browser will know which is keyword and which is data, string etc. To do this datatypes are used. Data types classify the data into "NUMBER", "STRING", "BOOLEAN" etc.
+
+// In chrome open "Developer tool" in that click on "Console", there i ran these codes.
+2+3
+5
+
+alert(2+3);
+undefined
+
+typeof(123);          // "typeof" is used to get datatype of specific data.
+'number'
+
+typeof("Hello");
+'string'
+
+typeof(true);
+'boolean'
