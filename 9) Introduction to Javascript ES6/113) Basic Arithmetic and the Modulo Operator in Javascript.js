71c1739c34ae9a311e81b2c801655b7e4aa878ba
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-16 16:13:50 +0530

    Create 113) Basic Arithmetic and the Modulo Operator in Javascript.js

---
+++ 9) Introduction to Javascript ES6/113) Basic Arithmetic and the Modulo Operator in Javascript.js
@@ -0,0 +1,4 @@
+// This is the code to convert dog age into human age.
+var dogAge= prompt("How old is your dog?");
+var humanAge = ((dogAge-2)*4)+21;
+alert("Your dog is "+humanAge+" years old in human years.");
