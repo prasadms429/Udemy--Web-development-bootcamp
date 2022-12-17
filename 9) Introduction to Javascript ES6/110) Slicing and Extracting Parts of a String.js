7586e5ad92ef5bd939aef1c0c21c56a6edc3d027
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-17 08:31:31 +0530

    Update 110) Slicing and Extracting Parts of a String.js

--- 9) Introduction to Javascript ES6/110) Slicing and Extracting Parts of a String.js
+++ 9) Introduction to Javascript ES6/110) Slicing and Extracting Parts of a String.js
@@ -1,9 +1,9 @@
-var name = "Prasad";
-name.slice(4,6);      // slice is used to take only specific characters that we want from string.
+var name = "Prasad";   // In programming, Position of characters start from zero.
+name.slice(4,6);      // slice is used to take only specific characters that we want from string. In this case start from 4th position and go till 6th position but not include that. It will print only "ad".
 
-// This code is to give an alert, with only 140 characters or below. If paragraph exceds more than 140 characters, then it will cut those characters automatically and display only 140 characters.
+// This code is to give an alert, with only 140 characters or below. If paragraph exceds more than 140 characters, then it will cut those characters automatically and give an alert of only 140 characters.
 var tweet= prompt("Compose your tweet:");   // This is to get input from user.
-tweetUnder140 = tweet.slice(0,140);
+tweetUnder140 = tweet.slice(0,140);         // In slice, to count how many characters included, substract upper value from lower value. i.e, 140-0 = 140. In name.slice(4,6), 6-4 = 2.
 alert(tweetUnder140);
 
 //This below code is another way of writing above code.
