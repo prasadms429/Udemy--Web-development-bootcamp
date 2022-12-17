9dfc71854892e68656b407709b7fc2ba6881ffa1
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-17 08:14:50 +0530

    Update 104) Javascript Variables.js

--- 9) Introduction to Javascript ES6/104) Javascript Variables.js
+++ 9) Introduction to Javascript ES6/104) Javascript Variables.js
@@ -1,12 +1,12 @@
-// To commit data in memory, we use variable. The name variable is used because, we can change data inside the variable. If use variable then data is saved in computer memory, otherwise we will loose it.
+// To commit data in memory, we use variable. The name, variable is used because, we can change data inside the variable. If we use variable then data is saved in computer memory, otherwise we will loose it.
 
-var myName = "Prasad";    // In this "var" is Keyword, "myName" is Name to variable and "Prasad" is Value. Let's assume Variable is a box, for that we assign name myName and inside that box, we store value Prasad.
+var myName = "Prasad";    // In this "var" is Keyword, "myName" is Name of variable and "Prasad" is Value. Let's assume Variable is a box, for that we assign name myName and inside that box, we store value Prasad.
 
 alert(myName);
 
-prompt("what is your name ?");      // "prompt" give interface to enter data.
+prompt("what is your name ?");      // "prompt" give interface to enter data. In this case, if we enter any data then it is not saved.
 
-var yourName = prompt("What is your Name ?");
+var yourName = prompt("What is your Name ?");   // we gave variable "yourName" to store data that is entered in prompt.
 
 alert("My name is "+myName+", Welcome to chennai "+yourName+"!");
 
