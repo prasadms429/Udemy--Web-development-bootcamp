51babc0baa646883e54e97835aada507d476e908
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-17 10:12:29 +0530

    Create 115) Functions Part 1: Creating and Calling Functions.js

---
+++ 9) Introduction to Javascript ES6/115) Functions Part 1: Creating and Calling Functions.js
@@ -0,0 +1,50 @@
+// Creating the function
+function getMilk() {      // we use function do repeated task or work. Lets assume fuction is one category of boxs, In that getMilk is name of one box, inside that we store block of code.
+  alert("leaveHouse");
+  alert("moveRight");
+  alert("moveRight");
+  alert("moveUp");
+  alert("moveUp");
+  alert("moveUp");
+  alert("moveUp");
+  alert("moveRight");
+  alert("moveRight");
+  alert("buyMilk");
+  alert("moveLeft");
+  alert("moveLeft");
+  alert("moveDown");
+  alert("moveDown");
+  alert("moveDown");
+  alert("moveDown");
+  alert("moveLeft");
+  alert("moveLeft");
+  alert("enterHouse");
+}
+
+// Calling the function
+getMilk();
+
+// This littel Robot program to get milk from a shop.
+function getMilk() {      
+  console.log("leaveHouse");      // "console.log" log the string inside the console. "console" is for developers, whereas "alert" is for user.
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveRight");
+  console.log("buyMilk");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("moveLeft");
+  console.log("enterHouse");
+}
+
+getMilk();
