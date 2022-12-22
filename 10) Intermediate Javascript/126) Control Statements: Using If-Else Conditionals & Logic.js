f8c108bac66c74c45a3e31978412ebdb9c7988ce
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-22 16:26:17 +0530

    Create 126) Control Statements: Using If-Else Conditionals & Logic.js

---
+++ 10) Intermediate Javascript/126) Control Statements: Using If-Else Conditionals & Logic.js
@@ -0,0 +1,13 @@
+// prompt("Enter your name:");
+// prompt("Enter your their name:");
+
+// var loveScore = Math.random();
+// loveScore = loveScore*100;
+// loveScore = Math.floor(loveScore) + 1;
+
+if(loveScore>70){       // In javascript equal to operator used inside conditional statement is " === ". ( Three equals )
+  alert("Your love score is "+loveScore+"%. You love each other like Kanye loves Kanye");
+}
+else{
+  alert("Your love score is "+loveScore+"%");
+}
