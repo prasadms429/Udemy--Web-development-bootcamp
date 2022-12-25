68738597b56f726026280a0ee55f6eb2edb671a9
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-25 17:28:02 +0530

    Create 127) Comparators and Equality.js

---
+++ 10) Intermediate Javascript/127) Comparators and Equality.js
@@ -0,0 +1,26 @@
+// In console of developer of tools.
+var a = 1;
+
+var b = "1";
+
+typeof(a)
+'number'
+
+typeof(b)
+'string'
+
+if(a === b){              // Thrible equal gives true only if datatypes and values of both side are equal.
+    console.log("Yes");   // To get more lines to write code, press shift+Enter on Console.
+}
+else{
+    console.log("No");
+}
+No
+
+if(a == b){               // Double equal gives true, if values of both sides are equal, regardless of their datatypes.
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
+Yes
