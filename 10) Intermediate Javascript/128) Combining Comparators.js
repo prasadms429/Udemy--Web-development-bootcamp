fc4e36fa63edf9568594fec5f061bd8d40578614
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-25 17:41:41 +0530

    Create 128) Combining Comparators.js

---
+++ 10) Intermediate Javascript/128) Combining Comparators.js
@@ -0,0 +1,13 @@
+if(loveScore>70){
+  alert("Your love score is "+loveScore+"%. You love each other like Kanye loves Kanye");
+}
+
+if(loveScore > 30 && loveScore <=70){
+  alert("Your love score is "+loveScore+"%");
+}
+  
+if(loveScore<=30) {
+  alert("Your love score is "+loveScore+"%. You go together like oil and water.");
+}
+
+// "!==" This is not equal to operator in javascript.
