0f11369facbf8df90c5b7689e3c0ffb2c9076544
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-25 20:25:14 +0530

    Update 130) Leap year solution.js

--- 10) Intermediate Javascript/130) Leap year solution.js
+++ 10) Intermediate Javascript/130) Leap year solution.js
@@ -36,12 +36,31 @@
 
 
 // Solution:
-function isLeap(year) {    
-    
+function isLeap(year) {       
      if((year%4 !== 0) || (year%100 === 0 && year%400 !== 0 ))
     {
         return "Not leap year.";
     }
     return "Leap year.";
+}
 
+// Another method of writing above code :
+function isLeap(year) {
+    if(year % 4==0){
+        if(year%100 === 0){
+            if(year%400===0){
+                return "Leap year.";
+            }
+            else{
+                return "Not leap year.";
+            }
+        }
+        else{
+            return "Leap year.";
+        }
+        
+    }
+    else{
+        return "Not leap year.";
+    }
 }
