e7826effb5545937855a07af62f18ba8632b0019
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-25 18:13:59 +0530

    Create Coding Exercise 5: BMI Calculator Advanced (IF or Else).js

---
+++ 10) Intermediate Javascript/Coding Exercise 5: BMI Calculator Advanced (IF or Else).js
@@ -0,0 +1,12 @@
+function bmiCalculator (weight, height) {
+    var bmi= Math.round(weight/Math.pow(height,2));
+    if (bmi < 18.5){
+      return "Your BMI is "+bmi+", so you are underweight."
+    }
+    if (bmi >= 18.5 && bmi <= 24.9){
+      return "Your BMI is "+bmi+", so you have a normal weight."
+    }
+    if (bmi > 24.9){
+      return "Your BMI is "+bmi+", so you are overweight."
+    }
+}
