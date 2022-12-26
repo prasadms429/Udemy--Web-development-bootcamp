fd5aa4e4df11ff1bdd95006df42b580eecf3e363
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2022-12-26 11:06:20 +0530

    Create 132) Adding Elements and Intermediate Array Techniques.js

---
+++ 10) Intermediate Javascript/132) Adding Elements and Intermediate Array Techniques.js
@@ -0,0 +1,24 @@
+// This program prints sequence of numbers from 1. Also this Program prints "fizz", when a number is divisible by 3 and It prints "buzz", when number is divisible by 5.
+// When a number is divisible by both 3 and 5, then it prints "fizzBuzz".
+
+var output = [];
+var num = 1;
+
+function fizzBuzz() {
+    if(num%3===0 && num%5===0){
+        output.push("fizzBuzz");
+    }
+    else if(num%3===0){
+        output.push("fizz");
+    }
+    else if (num%5===0) {
+        output.push("buzz");
+    }
+    else{
+        output.push(num);
+    }
+    
+    num=num+1;
+
+    console.log(output);
+}
