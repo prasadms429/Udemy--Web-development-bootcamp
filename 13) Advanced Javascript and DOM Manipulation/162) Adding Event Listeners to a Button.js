4e5bf8049a2f756abde933d0936acc9273c3d4bd
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-01-07 14:19:22 +0530

    Create 162) Adding Event Listeners to a Button.js

---
+++ 13) Advanced Javascript and DOM Manipulation/162) Adding Event Listeners to a Button.js
@@ -0,0 +1,17 @@
+//index.html
+<script src="index.js" charset="utf-8"></script>    // I added this line of code at the end of body section.
+
+// In index.js
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;    // To get total number of drum classes.
+for(var i=0;i<numberOfDrumButtons;i++){     // This code is to add click property to all buttons that contains "drum" class.
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){    // "addEventListener" is to do some function when a button is clicked. When a button is clicked then the dummy function, "function()" is get executed.
+    alert("I got clicked");                                                     // In the above code "click" is the one type of Event and "function()" is listener.  
+  });
+}
+
+// // Another way writinng line number 7 code.
+// document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);    // we dont use paranthesis for this function "hadleClick" inside "addEventListener". Beacuse, If we use paranthesis "()" with "handleClick" inside "addEventListener", then function get executed before clicking the button and alert message showed up before clicking button.
+
+// function handleClick(){
+//     alert("I got clicked");
+//   }
