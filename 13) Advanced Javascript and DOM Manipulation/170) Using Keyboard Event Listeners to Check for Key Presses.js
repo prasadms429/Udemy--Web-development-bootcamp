2297015dd9779bc814ad5cd05c79d64f4518449a
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-01-12 08:39:53 +0530

    Update 170) Using Keyboard Event Listeners to Check for Key Presses.js

--- 13) Advanced Javascript and DOM Manipulation/170) Using Keyboard Event Listeners to Check for Key Presses.js
+++ 13) Advanced Javascript and DOM Manipulation/170) Using Keyboard Event Listeners to Check for Key Presses.js
@@ -1,7 +1,7 @@
 // Detecting Button Press
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i=0;i<numberOfDrumButtons;i++){
-  document.querySelectorAll(".drum")[i].addEventListener("click",function(){var buttonInnerHTML = this.innerHTML;
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
   });
