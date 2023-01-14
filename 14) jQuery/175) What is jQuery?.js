9c24390359bededf3889dde1f0fb97022a282984
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-01-14 11:17:25 +0530

    Create 175) What is jQuery?.js

---
+++ 14) jQuery/175) What is jQuery?.js
@@ -0,0 +1,8 @@
+// jQuery is library of javascript. It is the simplest or shortest form of writing DOM javascript.
+// For example :-
+document.querySelector("h1");  // It is in DOM javascript.
+
+//In jQuery above code can be written as
+jQuery("h1");
+// OR more simplest way as below.
+$("h1");
