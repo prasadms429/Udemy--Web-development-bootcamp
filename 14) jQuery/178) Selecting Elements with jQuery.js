6b97e052cff619666507dd2fe237008c1b8af9ba
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-01-15 13:48:56 +0530

    Create 178) Selecting Elements with jQuery.js

---
+++ 14) jQuery/178) Selecting Elements with jQuery.js
@@ -0,0 +1,8 @@
+document.querySelector("h1");
+$("h1");    // short form of above code in jQuery. we can select css selector such as "h1" to modify.
+
+$("h1.title");      // we can select like this to manipulate elements.
+$("#header h1");
+
+document.querySelectorAll("button");    // This select all buttons that is defined inside index.html.
+$("button");    // This also selects all buttons that is defined inside index.html. If it is single or more, the syntax is same in jQuery.
