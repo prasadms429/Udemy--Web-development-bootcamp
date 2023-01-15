786732ca52db973f57d5a013278955182e9f1d74
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-01-15 13:17:26 +0530

    Create 177) How Minification Works to Reduce File Size.js

---
+++ 14) jQuery/177) How Minification Works to Reduce File Size.js
@@ -0,0 +1,2 @@
+// Minification is to reduce file size by removing spaces, new line and comments. In jQuery and Bootstrap library we can see minification of code.
+// The link for minification site is "www.https://www.minifier.org/".
