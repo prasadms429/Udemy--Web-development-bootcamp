ede358afdab498da1447fb09739a5e9222a4c7f0
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-03-22 13:06:12 +0530

    Update 182) Adding Event Listeners with jQuery.js

--- 14) jQuery/182) Adding Event Listeners with jQuery.js
+++ 14) jQuery/182) Adding Event Listeners with jQuery.js
@@ -12,3 +12,28 @@ for (var i=0; i<5; i++) {       // This block of code to change color of all but
 $("button").click(function(){       // By using jquery, above block of code is written as simple as this, for same purpose. When I click on any button on website, then it will change color of h1 element to purple.
   $("h1").css("color","purple");
 })
+
+
+// In index.html
+<input type="text" name="" value="">      // To add rectangle type box in a website, to receive inputs.
+
+  
+// In index.js
+$("input").keypress(function(event){    // This will log the key pressed on input into console.
+  console.log(event.key);
+})
+
+$(document).keypress(function(event){   // If we press key anywhere in the website, also it will log into console.
+  console.log(event.key);
+})
+
+$(document).keypress(function(event){   // This is to change h1 text to the keypressed on the keyboard.
+  $("h1").text(event.key);
+})
+
+$("h1").on("mouseover",function(){      // when I take my mouse or cursor on h1, then color of h1 change to purple. "on" method also works same as keypress or click, but it takes two inputs, one is event and another is function.
+  $("h1").css("color","purple");
+})
+
+// Link to different event's for "on" method
+https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseover_event
