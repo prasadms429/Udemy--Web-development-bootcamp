a882e73ed8ffffacfefcbf8625652c5ad0425e22
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-03-23 08:38:20 +0530

    Update 182) Adding Event Listeners with jQuery.js

--- 14) jQuery/182) Adding Event Listeners with jQuery.js
+++ 14) jQuery/182) Adding Event Listeners with jQuery.js
@@ -3,7 +3,7 @@ $("h1").click(function(){           // when I click on h1 element of website, th
   $("h1").css("color","purple");
 })
 
-for (var i=0; i<5; i++) {       // This block of code to change color of all buttons in a website to purple, when we click on any button. This block of code is basic javascript code, without jquery.
+for (var i=0; i<5; i++) {       // This block of code to change color of h1 in a website to purple, when we click on any button. This block of code is basic javascript code, without jquery.
   document.querySelectorAll("button")[i].addEventListner("click",function(){
     document.querySelector("h1").style.color = "purple";
   })
