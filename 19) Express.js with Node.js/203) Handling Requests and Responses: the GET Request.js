624efdfe664d6c213e1a56c72437377b602da973
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-04-15 23:43:48 +0530

    Create 203) Handling Requests and Responses: the GET Request.js

---
+++ 19) Express.js with Node.js/203) Handling Requests and Responses: the GET Request.js
@@ -0,0 +1,10 @@
+// In atom server.js
+app.get('/',function(request,response){     // "app.get()" get the request from browser and to send it server. This method takes two parameter. First one '/', this represents the location of Root(Server), where requested to be rooted.
+                                            // For second parameter we gave call back function, it gets triggered for both request from browser and response from server.
+  // console.log(request);          // This logs objects(information) related 'request' into console.
+  // response.send("Hello");        // It sends response to browser and prints "Hello" on browser.
+  response.send("<h1>Hello world!</h1>");   // This prints html h1 content on browser withought tags.
+});
+
+// In hyper
+node server.js
