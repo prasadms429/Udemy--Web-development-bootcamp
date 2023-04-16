9992a7aaf87ecf052ac3b3635fcd0e4ba2e5ca5c
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-04-16 23:05:41 +0530

    Create 209) Responding to Requests with HTML Files.js

---
+++ 19) Express.js with Node.js/209) Responding to Requests with HTML Files.js
@@ -0,0 +1,25 @@
+// In hyper
+touch index.html
+
+// In atom index.html
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Calculator</title>
+  </head>
+  <body>
+    <h1>Calculator</h1>
+    <form action="index.html" method="post">
+      <input type="text" name="num1" placeholder="First Number">      // "placeholder" is used to uniquely identify input.
+      <input type="text" name="num2" placeholder="Second Number">
+      <button type="submit" name="submit">Calculate</button>
+    </form>
+  </body>
+</html>
+
+// In atom calculator.js
+// This is used to send full html form into our browser, when it is requested.
+app.get('/',function(req,res){
+  res.sendFile(__dirname + "/index.html");    // "__dirname" gives location of current file, irrespective of whether it is hosted on cloud or it is on different computer or Remote location.
+});                                           // If someone else use my website then for them file location differs. So, we use this methodology.
