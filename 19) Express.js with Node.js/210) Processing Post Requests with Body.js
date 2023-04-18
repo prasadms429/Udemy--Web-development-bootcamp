65ca712df12e1d60ce0273c04b9a2fe76f373473
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-04-18 21:34:34 +0530

    Update 210) Processing Post Requests with Body.js

--- 19) Express.js with Node.js/210) Processing Post Requests with Body.js
+++ 19) Express.js with Node.js/210) Processing Post Requests with Body.js
@@ -12,3 +12,17 @@
 Cannot POST /
   
 // In Network section, If I click on error 404 (local host). Then click on "Headers", we can see Form data below.
+// num1 is the name bound to input1 here. num2 is the name bound to input2 here.
+
+// HTTP return code cheat sheet
+    1**. Hold on something gonna happen
+    2**. Here you go. This is usually a sucessful request code. (200- all ok)
+    3**. You screwed up. There is some security concern.
+    4**. Me screwed up  ( 400 - User uses browser to request something, that doesn't exist. )
+For more info-
+ https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+                         
+ // In atom calculator.js
+ app.post('/',function(req,res){            // "app.post" handles any post request coming to our Route. It accepts request from browser.
+  res.send("Thanks for posting that!");     // To reply to that request we send this.
+})      // Usually we use this to process the request from browser and send response.
