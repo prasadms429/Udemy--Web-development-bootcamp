a884ea2e22309e9b958df3e2e19993fd5a1f0e73
prasadms429 <71940251+prasadms429@users.noreply.github.com>
2023-04-20 20:31:07 +0530

    Update 210) Processing Post Requests with Body.js

--- 19) Express.js with Node.js/210) Processing Post Requests with Body.js
+++ 19) Express.js with Node.js/210) Processing Post Requests with Body.js
@@ -1,7 +1,7 @@
 // When I run server and give input in browser and click on calculate button on Calculator website. Then I get this error "Cannot POST /index.html".
 // click ctrl+shift+I and go to "Network" tab above there. Here we can see what error is. Now I'm getting error 404.
 
-// In calculator.js
+// In index.html
     <form action="/" method="post">         // method="post" means sending. To send input data to server, we use action="/" . Previously, it was sending our inputs to somewhere else.
       <input type="text" name="num1" placeholder="First Number">
       <input type="text" name="num2" placeholder="Second Number">
